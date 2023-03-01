a725f942302c6149d4beee4b23d25d803a1bb78a
WinWinMaw4 <mawinwinmaw4@gmail.com>
2023-03-01 11:58:20 +0630

    add google tag

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -85,6 +85,7 @@
       }
     </style>
 </head>
+<!-- Google tag (gtag.js) --><script async src="https://www.googletagmanager.com/gtag/js?id=UA-136018131-1"></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag('js', new Date());   gtag('config', 'UA-136018131-1');</script>
 <body>
 <!--Start Messenger -->
 
