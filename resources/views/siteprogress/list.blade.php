f28a21c72cc34ff61a6e14c819a2bee238cc774f
WinWinMaw <mawinwinmaw4@gmail.com>
2023-04-28 13:20:36 +0630

    scss

--- resources/views/siteprogress/list.blade.php
+++ resources/views/siteprogress/list.blade.php
@@ -14,7 +14,7 @@
         <!-- end back -->
     </div>
     <div class="container py-4 px-0 ">
-        <div class="row ">
+        <div class="row site-progress-detail">
             <!-- Site Progress -->
             <div class="col-12">
                 <div class="mb-4 d-flex align-items-center justify-content-between">
@@ -22,12 +22,11 @@
                 </div>
 
                 @foreach($siteProgresses as $siteProgress)
-                <div class="row row-cols-auto g-5  mb-3">
-                    <div class="col-12 px-5">
+                <div class="row row-cols-auto g-1 g-md-3  mb-3">
+                    <div class="col-12 px-0 px-md-3 px-lg-5">
                         <div class="row bg-white progressCard d-flex flex-row rounded rounded-3 overflow-hidden shadow-sm border" style="height:150px;">
                             <div class="col-4 col-md-3 col-lg-2 p-1 p-md-2">
                                 <div id="siteprogressId" class="siteprogressImage1 bg-secondary bg-opacity-50 d-flex justify-content-center align-items-center overflow-hidden position-relative h-100 rounded rounded-2 " style="width:100%;cursor:pointer">
-
 
                                     @foreach ($siteProgress->images as $key => $image)
                                     @if ($key == 0)
@@ -40,7 +39,7 @@
                             <div class="col-8 col-md-9 col-lg-10 d-flex align-items-baseline justify-content-start py-2">
 
                                 <div class="d-flex flex-column justify-content-center align-items-start w-100">
-                                    <div class="W-100 align-self-end">
+                                    <div class="w-100 align-self-end ">
                                         <div class="time  text-muted mb-1 ">
                                             <small class="">
                                                 <i class="bi bi-clock"></i>
